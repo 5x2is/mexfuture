8f768eb8a4560aff9f219e2c4828354938a278b2
ten <suitok1@gmail.com>
2020-04-13 21:57:32 +0900

    start

--- testCode.js
+++ testCode.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT;
-
-app.get('/', (req, res) => res.send('Welcome to Unubo Cloud!'));
-
-app.listen(PORT, () => console.log(`Ready on port`));
 console.log("aaa");
